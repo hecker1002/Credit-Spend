08470c67eb9c10ab85deb9860911abdcaba36e44
hecker1002 <polygot882@gmail.com>
2024-07-11 01:38:32 +0530

    did KMeans CLustering to reduce dimensiity and als categorise cities better as features .

--- test.py
+++ test.py
@@ -2,16 +2,26 @@ import streamlit as st
 import pickle 
 
 
-st.title( " My First App ")
-st.write("Hello World")
-st.image("D://Downloads//t1.png")
-st.radio( 'Select any one' , ['Male' , 'Female'])
-st.number_input( 'Number' , 0 , 100)
-# st.button('Predict')
+st.title( " My First App ") # web page title 
 
-if ( st.button('Predict')) : 
+st.write('Hello, Predict your this MOnth expenditure and we will suggeat you Savings ')
+
+st.image("money1.jpg")
+
+
+def imput_user() :
+    # take input from user and store in dict( key-value pair 
+    pass
+
+def output_user() :
+    # take input and give output form ML model in Backend 
+    pass
+
+
+# Only button should eb uesd with if(TRue) , no need to ceclare it sepletely 
+if ( st.button('PREDICT!')) : 
     # Load the modle , give the input 
     model = pickle.load( open('forest_reg.pkl' , 'rb'))
     sample  = [ [ 1  , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ,2014 , 10 , 0 , 126 ] ]
-    model.predict( sample)
-    st.write(model.predict( sample)[0])
+    prediction = model.predict( sample)
+    st.write(prediction[0])
