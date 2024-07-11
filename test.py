3b20bc3bd378f5c6ff60b7664297ec495a448ab6
hecker1002 <polygot882@gmail.com>
2024-07-11 13:35:51 +0530

    Now , ONly need to do Fine-tuning and Multivariate One-Step Time Forecasting .

--- test.py
+++ test.py
@@ -4,24 +4,25 @@ import pickle
 
 st.title( " My First App ") # web page title 
 
-st.write('Hello, Predict your this MOnth expenditure and we will suggeat you Savings ')
+st.write('Hello, Predict your this Month expenditure and we will suggeat you Savings ')
 
 st.image("money1.jpg")
 
 
 def imput_user() :
-    # take input from user and store in dict( key-value pair 
+    # take input from user and store in dict( key-value pair) 
     pass
 
 def output_user() :
     # take input and give output form ML model in Backend 
     pass
 
+model = pickle.load( open('forest_reg.pkl' , 'rb'))
 
 # Only button should eb uesd with if(TRue) , no need to ceclare it sepletely 
 if ( st.button('PREDICT!')) : 
     # Load the modle , give the input 
-    model = pickle.load( open('forest_reg.pkl' , 'rb'))
-    sample  = [ [ 1  , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ,2014 , 10 , 0 , 126 ] ]
+    
+    sample  = [ [1.00, 0.0, 0.0, 0.0, 2014 , 4.0 , 0.0, 1.0] ]
     prediction = model.predict( sample)
     st.write(prediction[0])
