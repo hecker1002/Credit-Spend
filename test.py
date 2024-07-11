2865accf8bc38843bfbf8e900e319c3798a1bc33
hecker1002 <polygot882@gmail.com>
2024-07-12 02:35:28 +0530

    haalf completed Streamlit Web App

--- test.py
+++ test.py
@@ -8,10 +8,24 @@ st.write('Hello, Predict your this Month expenditure and we will suggeat you Sav
 
 st.image("money1.jpg")
 
+card = st.selectbox('Pick your Card Type', ['Gold' , 'Platinum' , 'Signature' , 'Silver'])
 
-def imput_user() :
-    # take input from user and store in dict( key-value pair) 
-    pass
+month = st.selectbox('Pick the Month', ['January','February','MArch','April' ,'May','June',
+                                'July','August','September','OCtober','November','December'])
+
+gender = st.selectbox('Gender', ['Male','Female'])
+
+city = st.selectbox( 'City' , ['1','2'])
+
+st.progress(5)
+
+# Map the values to dictionary 
+
+Cards = {'Gold':[1,0,0,0], 'Platinum':[0,1,0,0], 'Signature':[0,0,1,0], 'Silver':[0,0,0,1]}
+
+Genders = { 'Male':0 , 'Female':1 }
+
+
 
 def output_user() :
     # take input and give output form ML model in Backend 
@@ -19,10 +33,17 @@ def output_user() :
 
 model = pickle.load( open('forest_reg.pkl' , 'rb'))
 
+app_mode = st.siderbar.selectbox('Select PAge' , ['Predict Transaction' ,'Monthly Expenditure'])
+
+
 # Only button should eb uesd with if(TRue) , no need to ceclare it sepletely 
 if ( st.button('PREDICT!')) : 
     # Load the modle , give the input 
     
-    sample  = [ [1.00, 0.0, 0.0, 0.0, 2014 , 4.0 , 0.0, 1.0] ]
+    sample  = [ Cards[card], 2014 , 4.0 , 0.0, Genders[gender] ] 
+
     prediction = model.predict( sample)
-    st.write(prediction[0])
+    st.success(prediction[0])
+    st.image("gender_.png")
+
+
